89943e71617b54c32bc6ecc077a3287dd5ad931e
Unknown <chrisentem@gmail.com>
2018-07-09 15:57:04 +0200

    File indentation optimized

--- View/Admin/editPost.php
+++ View/Admin/editPost.php
@@ -8,34 +8,33 @@
 ?>
 
 <h1>Administration - Modifier un billet</h1>
-    	<div class="row">
-    		<div class="col-md-6">
-		    	<form action="<?= 'admin/savePost/' . $this->clean($post['id']) ?>" method="post">
-		    		<div class="form-group">
-		    			<label for="title">Votre titre</label>
-						<input type="text" class="form-control" name="title" id="title" value="<?= $this->clean($post['title']) ?>" />
-					</div>
-					<div class="form-group">
-						<label for="content">Votre texte</label>
-						<textarea rows="3" class="admin-editor" id="content" name="content"/><?= $this->clean($post['content']) ?></textarea>
-					</div>
-					<div class="form-group">
-						<button type="submit" class="btn btn-primary">Sauvegarder</button>
-					</div>
-				</form>
-				<a class="btn btn-secondary" href="javascript:history.back()">Retour</a>
-        	</div>
-    		<div class="col-md-6">
-    			<div class="row">
-    				<div class="col-12">
-    					<span style="color: #bfbfbf; font-size: 1rem; font-weight: 400;">Aperçu du billet</span>
-    				</div>
-    				<div class="admin-edit col-12">
-				        <h3><?= $this->clean($post['title']) ?></h3>
-				        <time>Crée le : <?= $this->clean($post['date']) ?></time>
-				        <time><?= !is_null($post['modificationDate']) ? ' - Modifié le : ' . $this->clean($post['modificationDate']) : '' ?></time><hr>
-				        <p><?= $post['content'] ?></p>
-				    </div>
-		    	</div>
-		    </div>
-        </div>		
+<div class="row">
+	<div class="col-md-6">
+		<form action="<?= 'admin/savePost/' . $this->clean($post['id']) ?>" method="post">
+			<div class="form-group">
+				<label for="title">Votre titre</label>
+				<input type="text" class="form-control" name="title" id="title" value="<?= $this->clean($post['title']) ?>" />
+			</div>
+			<div class="form-group">
+				<label for="content">Votre texte</label>
+				<textarea rows="3" class="admin-editor" id="content" name="content"/><?= $this->clean($post['content']) ?></textarea>
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Sauvegarder</button>
+			</div>
+		</form>
+	</div>
+	<div class="col-md-6">
+		<div class="row">
+			<div class="col-12">
+				<span style="color: #bfbfbf; font-size: 1rem; font-weight: 400;">Aperçu du billet</span>
+			</div>
+			<div class="admin-edit col-12">
+				<h3><?= $this->clean($post['title']) ?></h3>
+				<time>Crée le : <?= $this->clean($post['date']) ?></time>
+				<time><?= !is_null($post['modificationDate']) ? ' - Modifié le : ' . $this->clean($post['modificationDate']) : '' ?></time><hr>
+				<p><?= $post['content'] ?></p>
+			</div>
+		</div>
+	</div>
+</div>		
